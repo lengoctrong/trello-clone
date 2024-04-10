fdf0f219c4bb81f328cab707c9bacff1d131f6d0
Trong Le <asurakul205@gmail.com>
2024-04-10 16:07:02 +0700

    drag column

--- src/features/boards/Board.jsx
+++ src/features/boards/Board.jsx
@@ -1,12 +1,27 @@
+import { DndContext } from '@dnd-kit/core'
+import { moreIcon } from '~/icons'
 import ListColumns from '../columns/ListColumns'
-import Header from './Header'
 
 const Board = () => {
+  const handleDragEnd = (e) => {
+    console.log(e)
+  }
+
   return (
-    <div className="h-[calc(100%-48px)] px-8">
-      <Header />
-      <ListColumns />
-    </div>
+    <DndContext onDragEnd={handleDragEnd}>
+      <div className="h-[calc(100%-48px)] px-8">
+        <div className="flex justify-between py-3">
+          <input
+            className=" btn font-medium outline-blue-500"
+            value={'Board Title'}
+            size={6}
+            onChange={() => {}}
+          />
+          <button className="btn items-center bg-white">{moreIcon}</button>
+        </div>
+        <ListColumns />
+      </div>
+    </DndContext>
   )
 }
 export default Board
